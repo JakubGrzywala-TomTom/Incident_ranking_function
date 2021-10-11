41a77541616342a679b1cdb5bd8b3e6c548548a0
Jakub Grzywala <jakub.grzywala@tomtom.com>
2021-10-11 13:25:04 +0200

    input-description.txt file which helps in composing an input.json file, describes  purpose of every field, type of values it accepts, how can different values change the output. Also added little modifications to plot_maker.

--- plot_maker.py
+++ plot_maker.py
@@ -10,6 +10,7 @@ def create_histogram(series: pd.Series,
                      limit: int) -> None:
     series_max_boundary = 1
     series_min_boundary = round(series.min(), 1)
+    num_of_messages = series.count()
     if series_min_boundary >= 0:
         bin_num = int((series_max_boundary + series_min_boundary) * 100)
     else:
@@ -26,13 +27,14 @@ def create_histogram(series: pd.Series,
     ax.grid(color='#000000', linestyle="--")
     ax.axvline(ranking_score_capping, color="#df1b12", linestyle="--", linewidth=3,
                label=f"capping value ({ranking_score_capping})")
-    if limit <= series.count():
+    if limit <= num_of_messages:
         limit_score_value = series.iloc[limit]
         ax.axvline(limit_score_value, color="#fdc530", linestyle="--", linewidth=3,
                    label=f"limit from configuration file ({limit})"
                          f"\nscore of last message before the limit is ~ {round(limit_score_value, 3)}")
     ax.legend(loc='upper right', fontsize=10, shadow=True, facecolor='#ffffff')
-    ax.set_title("Unfiltered messages, sorted by ranking score, before using capping and limit", fontsize=10)
+    ax.set_title(f"Unfiltered messages ({num_of_messages}), sorted by ranking score, before using capping and limit",
+                 fontsize=10)
     ax.set_xlabel("Ranking score", fontsize=10)
     ax.set_ylabel("Num of messages in 0.01 bins", fontsize=10)
     fig.savefig(join(cwd + sep + 'histogram.png'))
